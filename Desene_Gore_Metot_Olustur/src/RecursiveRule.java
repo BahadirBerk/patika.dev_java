07562c202424108e10efdfe58ea1875f9a9d1864
Bahadır Berk <37116128+BahadirBerk@users.noreply.github.com>
2023-06-28 13:05:28 +0300

    Add files via upload

---
+++ Desene_Gore_Metot_Olustur/src/RecursiveRule.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursiveRule {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int n = scan.nextInt();
+
+        recursiveRule(n);
+    }
+
+    static void recursiveRule(int n) {
+        System.out.println(n);
+
+        if (n <= 0) {
+            return;
+        }
+
+        recursiveRule(n - 5);
+        System.out.println(n);
+    }
+}
